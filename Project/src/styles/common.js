3dfb61af2496177315c7f1c1e7967a22f7b8b364
hahahaday12 <hahahaday12@naver.com>
2023-01-18 00:00:03 +0900

    Inventory Management-Project 공통 css 함수

---
+++ Project/src/styles/common.js
@@ -0,0 +1,25 @@
+import { css } from 'styled-components';
+
+export const Background = css`
+  //width: 300px;
+  height: 30px;
+  display: flex;
+  background-color: white;
+  position: absolute;
+  font-weight: 600;
+`
+export const CommonLayout3 = css`
+  width: 350px;
+  height: 60px;
+  background-color: white;
+  display: flex;
+  position: relative;
+  border-radius: 10px;
+`
+export const InnerText3 = css`
+  width: 280px;
+  height: 30px;
+  margin-top: 18px;
+  margin-left: 55px;
+  font-weight: 600;
+`
