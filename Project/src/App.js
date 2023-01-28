af28ff303cae22d8f997e21fd2bca75e725ebd8f
hahahaday12 <hahahaday12@naver.com>
2023-01-28 15:52:32 +0900

    📁Page Router 설정
    - refresh token값 백엔드에 post 형식으로 전달.
    - 백엔드에서 accessToken을 받은후 localStorge 저장.
    - token을  Header에 넣음.
    - store에 저장한 localStorge token의 정보 가져옴.

--- Project/src/App.js
+++ Project/src/App.js
@@ -1,26 +1,38 @@
-import './App.css';
-import FirstMain from './pages/Home/Main/index';
 import { Routes, Route, Router} from 'react-router-dom';
-import Service from './ServicePage';
+import FirstMain from './pages/Home/Main/index';
+import LoginText from './pages/Form/components/login/loginForm';
+import JoinForm from './pages/Form/components/Join/joinForm'
+import ProuctLayout from './pages/Products/Common';
+import PrivateRoute from './privateRoute';
+import { useDispatch } from 'react-redux';
+import { SET_TOKEN } from './Store/Auth';
+import './App.css';
 
 function App() {
+  const dispatch = useDispatch();
+    useEffect(() =>{
+     console.log("APP");
+ 		  axios.post('http://localhost:3001/auth/silent-refresh',{}, {
+ 			withCredentials : true
+ 		}).then(res => {
+ 		  console.log(JSON.stringify(res.data));
+      if(res.data.accessToken){
+        window.localStorage.setItem("accessToken",res.data.accessToken);
+        axios.defaults.headers.common['Authorization'] = 'Bearer ' + res.data.accessToken;
+        dispatch(SET_TOKEN(res.data.accessToken));
+        console.log("set accessToken",res.data.accessToken);
+      }
+ 	  });
+  },[]);
+
   return (
-    <div className='App'>
-    <>
-   
-      <Routes>
-        <Route path="/" element={
-            <PrivateRoute component={<Service/>} status={<FirstMain/>} />
-          }/>
-        <Route path="/service" element={
-            <PrivateRoute component={<Service/>} />
-          }/>
-      </Routes>
-     
-    </>
-    </div>
-  
+    <Routes>
+      <Route path="/" element={<FirstMain/>}/>
+      <Route path="/prouct" element={<PrivateRoute component={<ProuctLayout/>}/>
+      }/>
+      <Route path='/login' element={<LoginText/>}/>
+      <Route path='/join' element={<JoinForm/>}/>
+    </Routes>
   );
 }
-
 export default App;
