9c3a443e33ed11e1ff04ff0731736faddb97f57d
hahahaday12 <hahahaday12@naver.com>
2023-01-28 16:11:25 +0900

    ⚙Inventory Management-Project 공통 Layout

---
+++ Project/src/common/Layout/commonlayout.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const ALLlayout = () => {
+  return(
+    <CommonLayout>
+    </CommonLayout>
+  )
+};
+export default ALLlayout;
+
+const CommonLayout = styled.div`
+  width: 1536px;
+  height: 864px;
+  background-color: aliceblue;
+`
