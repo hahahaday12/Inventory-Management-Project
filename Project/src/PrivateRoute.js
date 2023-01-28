17795459c08d16d553a8c02f4a0e0ffd7b4c337a
hahahaday12 <hahahaday12@naver.com>
2023-01-28 16:03:02 +0900

    📁Private Page 설정
    -  useSelector 사용하여 accessToken 가져옴
    - 가져온 accessToken  통하여 상태값 체크후 접근권한 판단.

--- Project/src/PrivateRoute.js
+++ Project/src/PrivateRoute.js
@@ -1,23 +1,20 @@
-import React from 'react';
-import axios from 'axios';
 import { Navigate } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import React from 'react';
  
-function PrivateRoute({ component:Component, status:Status }) {
-      let result;
-      let check = false;
-      const accessToken = localStorage.getItem('token');
-		
-			if(accessToken){
-				axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
-			}
-	
-      if(check){
-        result = Component;
-      }else{
-        !Status ?  result = <Navigate to='/'{...alert("접근할수 없는 페이지 입니다.")}/> : result = Status
-      }
-      return result;
- }
-
+const PrivateRoute = ({ component:Component, status:Status }) =>  {
+  const { accessToken } = useSelector(state => state.authToken);
+    let check = false;
+    let result = null;
+	  
+    if(accessToken){
+      check = true;
+    }
+    if(check){
+      result = Component;
+    }else{
+        !Status ? result = <Navigate to='/'{...alert("접근할수 없는 페이지 입니다.")}/> : result = Status
+    }
+    return result;
+ };
  export default PrivateRoute
