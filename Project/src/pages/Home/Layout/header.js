1dafb10ac6e167151816cbd2260ae58070729a04
hahahaday12 <hahahaday12@naver.com>
2023-01-29 23:41:42 +0900

    📁Inventory Management-Project Layout
    - Home Page layout 추가
    - Home Page header 추가

---
+++ Project/src/pages/Home/Layout/header.js
@@ -0,0 +1,63 @@
+import HeaderLoginButton  from '../Button/button';
+import LogoutButton from '../Button/logoutboutton';
+import { useSelector } from 'react-redux';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const LayoutHeader = () => {
+  const { accessToken } = useSelector(state => state.authToken);
+  console.log("accessToken",accessToken);
+
+  return(
+    <>
+      <HeaderContainer>
+        <img alt='logoimg' src='./img/boxlog.png'/>
+        <CategoryBox>
+          <InnerBox>
+            <Link style={{ textDecoration: 'none', color: 'gray' , fontWeight: '600' }} to="/service">서비스</Link>
+            <Link style={{ textDecoration: 'none', color: 'gray' , fontWeight: '600' }} to="/service">요금안내</Link>
+            <Link style={{ textDecoration: 'none', color: 'gray' , fontWeight: '600' }} to="/prouct">리소스</Link>
+          </InnerBox>
+            {accessToken != null ? <LogoutButton/> : <HeaderLoginButton/>}  
+        </CategoryBox>
+      </HeaderContainer>
+    </>
+  )
+};
+export default LayoutHeader
+
+const HeaderContainer = styled.div`
+  width: 1536px;
+  height: 80px;
+  background-color: white;
+  display:flex;
+  justify-content: space-between;
+  position: fixed;
+  top: -1px;
+
+  & img{
+    width: 130px;
+    height: 130px;
+    display: flex;
+    position: relative;
+    bottom:20px;
+    margin-left: 200px;
+  }
+`
+const CategoryBox = styled.div`
+  width: 500px;
+  height: 60px;
+  background-color: white;
+  display: flex;
+  position: relative;
+  right: 100px;
+  margin-top: 10px;
+`
+const InnerBox = styled.div`
+  width: 400px;
+  height: 20px;
+  gap: 70px;
+  display: flex;
+  position: relative;
+  top: 20px;
+`
---
+++ Project/src/pages/Home/Layout/layout.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+import LayoutHeader from './header'
+
+const Main = () => {
+  return(
+    <CommonLayout>
+      <LayoutHeader/>
+    </CommonLayout>
+  )
+};
+export default Main;
+
+const CommonLayout = styled.div`
+  width: 1536px;
+  height: 3380px;
+  background-color: white;
+`
