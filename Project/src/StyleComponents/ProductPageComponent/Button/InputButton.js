a4fcf78c6f63cffca86aae27a4e6c552fad590b0
hahahaday12 <hahahaday12@naver.com>
2023-02-06 23:58:56 +0900

    ⚙Inventory Management-Project ProductPage
    - Button Components 추가

---
+++ Project/src/StyleComponents/ProductPageComponent/Button/InputButton.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import { Button } from '../InOutputStyle/style';
+
+
+const InputButton = () => {
+  return(
+    <>
+    <Button>입고</Button>
+    </>
+  )
+};
+export default InputButton;
---
+++ Project/src/StyleComponents/ProductPageComponent/Button/OutButton.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import { Button } from '../InOutputStyle/style';
+
+
+const OutButton = () => {
+  return(
+    <>
+    <Button>출고</Button>
+    </>
+  )
+};
+export default OutButton;
