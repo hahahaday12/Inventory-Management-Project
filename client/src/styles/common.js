afbcf0642da53ad3ca96ed6c97c09ce8e226d8f0
hahahaday12 <hahahaday12@naver.com>
2023-03-03 00:50:43 +0900

    💄 : Inventory Management-Project Style

---
+++ client/src/styles/common.js
@@ -0,0 +1,48 @@
+import { css } from 'styled-components';
+
+export const Background = css`
+  height: 30px;
+  display: flex;
+  background-color: white;
+  position: absolute;
+  font-weight: 600;
+`
+export const CommonLayout3 = css`
+  width: 350px;
+  height: 60px;
+  background-color: white;
+  display: flex;
+  position: relative;
+  border-radius: 10px;
+`
+export const InnerText3 = css`
+  width: 280px;
+  height: 30px;
+  margin-top: 18px;
+  margin-left: 55px;
+  font-weight: 600;
+`
+export const FooterText = css`
+  width: 500px;
+  height: 80px;
+  display: flex;
+  position: absolute;
+  color: white;
+`
+
+export const FooterBoxx = css`
+  width: 900px;
+  height: 160px;
+  display: flex;
+  position: relative;
+`
+export const CommonLayoutAll = css`
+  display: flex;
+  position: relative;
+`
+export const FontStyle = css`
+  width: 230px;
+  height: 40px;
+  display: flex;
+  position: absolute;
+`
