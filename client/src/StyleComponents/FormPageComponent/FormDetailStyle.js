935664aef3d0e56ba8bdcb2bf43fa6997690bafe
hahahaday12 <hahahaday12@naver.com>
2023-03-03 00:52:18 +0900

    💄 : Inventory Management-Project StyleComponents
    - 폼 페이지 style

---
+++ client/src/StyleComponents/FormPageComponent/FormDetailStyle.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const FormButtonBox = styled.div`
+  width: 100px;
+  height: 45px;
+  background-color: white;
+  display: flex;
+  position: relative;
+  top: 8px;
+  right: 50px;
+  border-radius: 6px;
+  border: solid 1px black;
+`
+export const FormText = styled.div`
+   font-size:15px;
+   font-weight: 600;
+   width: 60px;
+   height: 30px;
+   display: flex;
+   align-items: center;
+   position: relative;
+   top: 8px;
+   margin-left: 20px;
+`
