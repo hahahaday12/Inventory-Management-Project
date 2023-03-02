e532c11e78229dcbafb505575ca23ee0ce5db687
hahahaday12 <hahahaday12@naver.com>
2023-03-03 00:18:35 +0900

    ⚙ Inventory Management-Project Setting

---
+++ client/src/index.js
@@ -0,0 +1,16 @@
+import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import React from 'react';
+import App from './App';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Provider store={store}>
+        <BrowserRouter>
+        <App />
+        </BrowserRouter>
+    </Provider>
+);
+
+
