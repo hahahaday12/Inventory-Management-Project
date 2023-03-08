19ff5e2c19c27594a8328d8bd2c4b5dcf66f97ce
hahahaday12 <hahahaday12@naver.com>
2023-03-08 23:49:31 +0900

    🎨 Refactor : Improve structure / format of the code.
    - import redux , store 추가

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,9 @@ import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import React from 'react';
 import App from './App';
-import './index.css';
+import { Provider } from 'react-redux'
+import store from './store';
+import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
