e7b859fe79819b7b07c3577042318dc5823da70f
hahahaday12 <hahahaday12@naver.com>
2023-03-21 23:54:06 +0900

    🎨 Refactor : Improve structure / format of the code.
    - App.js 토큰 코드 로직 수정

--- client/src/App.js
+++ client/src/App.js
@@ -14,21 +14,19 @@ import InputProducts from './pages/Products/InpustProducts/inputProductsPage';
 import './App.css';
 
 function App() {
-  const dispatch = useDispatch();
-    useEffect(() =>{
+  const [accessToken, setAccessToken] = useState(null);
+   useEffect(() =>{
      console.log("APP");
- 		  axios.post('http://localhost:3001/auth/silent-refresh',{}, {
+ 		axios.post('http://local host:3001/auth/silent-refresh',{}, {
  			withCredentials : true
  		}).then(res => {
  		  console.log(JSON.stringify(res.data));
       if(res.data.accessToken){
-        window.localStorage.setItem("accessToken",res.data.accessToken);
-        axios.defaults.headers.common['Authorization'] = 'Bearer ' + res.data.accessToken;
-        dispatch(SET_TOKEN(res.data.accessToken));
-        console.log("set accessToken",res.data.accessToken);
+         window.localStorage.setItem("accessToken",res.data.accessToken);
+         setAccessToken(res.data.accessToken);
       }
  	  });
-  },[]);
+   },[]);
 
   return (
     <Routes>
