4108ab652f519ccb93fa688f58d2d9746acc79c7
hahahaday12 <hahahaday12@naver.com>
2023-03-29 23:58:40 +0900

    🎨 : Store 코드 구조 개선
    - reducer 추가

--- client/src/Store/index.js
+++ client/src/Store/index.js
@@ -1,8 +1,29 @@
-import { configureStore } from '@reduxjs/toolkit';
-import tokenReducer from './Auth';
+import { configureStore,combineReducers } from '@reduxjs/toolkit';
+import tokenReducer from '../_reducers/Auth';
+import userReducer from '../_reducers/user'
+import { persistReducer } from 'redux-persist'
+import thunkMiddleware from 'redux-thunk';
+import storage from 'redux-persist/lib/storage';
 
-export default configureStore({
-    reducer: {
-        authToken: tokenReducer,
-    },
+
+const reducers = combineReducers({
+    authToken: tokenReducer,
+    user: userReducer,
 });
+
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const store = configureStore({
+    reducers,
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: false,
+    }).concat(thunkMiddleware),
+});
+
+export default store; 
